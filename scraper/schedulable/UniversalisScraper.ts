4b08d978e8adedbfefc330730c14890af6697123
Vic <victor.bartel@stootie.com>
2018-04-19 23:35:06 +0200

    Migrate from universalis to ewtn

--- scraper/schedulable/UniversalisScraper.ts
+++ scraper/schedulable/UniversalisScraper.ts
@@ -1,24 +1,47 @@
-import {getLinks, IEndpoint} from "./tools";
+import {getEwtnLinks, IEndpoint} from "./tools";
 import axios from "axios";
-import {get} from "lodash";
+import {get, isNil} from "lodash";
+import {load} from "cheerio";
+import { Endpoint } from "aws-sdk";
 
-export interface IScraper {
-    scrap(): Promise<any>;
+export interface ICrawler {
+    crawl(): Promise<any>;
 }
 
-export class UniversalisScraper implements IScraper {
+export class EwtnCrawler implements ICrawler {
 
-    constructor(readonly baseUrl: string = "http://universalis.com/") {}
+    private readingPattern = /(?:reading|psalm|gospel)/gi;
 
-    public async scrap() {
-        const responses = await Promise.all(getLinks(this.baseUrl).map(x => this.mapWithPageContent(x)));
-        console.log(responses);
+    public async crawl() {
+        const links = getEwtnLinks().map(x => this.mapWithPageContent(x));
+        const responses = await Promise.all(links).catch(e => null);
+        const result = responses
+            .map(x => this.extractReferences(x))
+            .filter(x => !isNil(x));
+        console.log(JSON.stringify(result));
+    }
+    private extractReferences(endpoint: IEndpoint) {
+        const color    = get(endpoint.data, "Color");
+        const readings = get(endpoint.data, "ReadingGroups[0].Readings", [])
+            .map(x => ({
+                reference: get(x, "Citations[0].Reference"),
+                type: get(x, "Type", "").replace(/ /g, ""),
+            }))
+            .map(x => isNil(x.reference) ? null : x )
+            .filter(x => !isNil(x));
+        return {color, readings, key: endpoint.key};
+        // console.log(title);
     }
 
     private async mapWithPageContent(endpoint: IEndpoint) {
-        const response = await axios.get(endpoint.url).catch(this.processError);
-        if (get(response, "statusText") !== "OK") { return null; }
-        return {html: response.data, ...endpoint};
+        try {
+            const response = await axios.get(endpoint.url).catch(this.processError);
+            if (get(response, "statusText") !== "OK") { return null; }
+            return {data: response.data, ...endpoint} as IEndpoint;
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
     }
 
     // noinspection JSMethodCanBeStatic
@@ -28,4 +51,4 @@ export class UniversalisScraper implements IScraper {
     }
 }
 
-(new UniversalisScraper().scrap());
+(new EwtnCrawler().crawl());
--- scraper/schedulable/tools.ts
+++ scraper/schedulable/tools.ts
@@ -1,54 +1,18 @@
 import {times, values, flow, flatMap, curryRight} from "lodash";
 import moment = require("moment");
+import { url } from "inspector";
 
-const getCountryCalendars = () => Object.freeze([
-    ["South America"                 , "americas"],
-    ["South America - Brazil"        , "americas.brazil"],
-    ["Asia"                          , "asia"],
-    ["Asia - India"                  , "asia.india"],
-    ["Asia - Singapore"              , "asia.singapore"],
-    ["Asia - Philippines"            , "philippines"],
-    ["Australia"                     , "australia"],
-    ["New Zealand"                   , "nz"],
-    ["North America - Canada"        , "canada"],
-    ["North America - United States" , "usa"],
-    ["Africa - Nigeria"              , "nigeria"],
-    ["Africa - Southern Africa"      , "safrica"],
-    ["Europe"                        , "europe"],
-    ["Europe - Belarus"              , "europe.belarus"],
-    ["Europe - England"              , "europe.england"],
-    ["Europe - Ireland"              , "europe.ireland"],
-    ["Europe - Italy"                , "europe.italy"],
-    ["Europe - Malta"                , "europe.malta"],
-    ["Europe - Netherlands"          , "europe.netherlands"],
-    ["Europe - Poland"               , "europe.poland"],
-    ["Europe - Scotland"             , "europe.scotland"],
-    ["Europe - Sweden"               , "europe.sweden"],
-    ["Europe - Wales"                , "europe.wales"],
-    ["Eastern General"               , "east"],
-    ["Middle East"                   , "meast"],
-    ["Middle East - Southern Arabia" , "meast.sarabia"],
-] as Array<[string, string]>);
-
-const getDates = (max: number = 7) =>
-    times(7, x => moment.utc().add(x, "days").format("YYYYMMDD"));
-
-// http://universalis.com/americas.brazil/20180413/mass.htm
+const getDates = (max: number = 30) =>
+    times(max, x => moment.utc().add(x, "days").format("YYYY-M-DD"));
 
 export interface IEndpoint {
     url: string;
-    title: string;
+    title?: string;
     key: string;
+    data?: any;
 }
 
-export const getLinks = (baseUrl: string): IEndpoint[] => {
-    const formatUrls: (x) => IEndpoint[] = x => getDates().map(y => ({
-        title: x[0],
-        key: `${x[1]}/${y}`,
-        url: `${baseUrl}/${x[1]}/${y}/mass.htm`,
-    }));
-    const chain = flow(getCountryCalendars, x => flatMap(x, formatUrls));
-    return chain();
+export const getEwtnLinks = (): IEndpoint[] => {
+    const baseUrl = "https://www.ewtn.com/se/readings/readingsservice.svc/day";
+    return getDates().map(x => ({url: `${baseUrl}/${x}/en`, key: `readings/ewtn/${x}/data.json`}));
 };
-
-// console.log(getLinks("http://universalis.com"));
